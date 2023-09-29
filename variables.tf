a0d1e966a27020133da28ad645cbfc9e980bd8f2
MarinoVP <Marinovp@users.noreply.github.com>
2023-09-29 16:33:23 -0400

    #25 refactor s3 bucket into new terrahouse_aws module (#26)

--- variables.tf
+++ variables.tf
@@ -1,17 +1,7 @@
 variable "user_uuid" {
-  description = "The UUID of the user"
   type        = string
-  validation {
-    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message    = "The user_uuid value is not a valid UUID."
-  }
 }
 
 variable "bucket_name" {
   type = string
-  description = "The name of the S3 bucket."
-  validation {
-    condition = can(regex("^[a-z0-9-]{1,63}$", var.bucket_name))
-    error_message = "The bucket_name must be a valid S3 bucket name. For more information, see https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html."
-  }
 }
