6c61a8b146a8336c57d9c146b2d03149f827d20a
MarinoVP <Marinovp@users.noreply.github.com>
2023-09-30 09:14:56 -0400

    27 s3 static website hosting (#28)
    * #27 Static Website hosting
    
    * #27 Enable static website hosting

--- module/terrahouse_aws/main.tf
+++ module/terrahouse_aws/main.tf
@@ -19,6 +19,40 @@ resource "aws_s3_bucket" "website_bucket" {
   tags = {
     UserUuid = var.user_uuid
   }
-
 }
 
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "website_index" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+   etag = "${md5(file(var.index_html_filepath))}"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = "${md5(file(var.error_html_filepath))}"
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket nanme for static website"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "Output of S3 Static Website"
+  value = module.terrahouse_aws.website_endpoint
+}
