285fc7e4a196ca054a05f12e2bfd7dfaea5954e5
Marino VP <marinovp21@gmail.com>
2023-09-30 16:49:55 +0000

    #29 CDN Implementation

--- module/terrahouse_aws/main.tf
+++ module/terrahouse_aws/main.tf
@@ -1,58 +1,11 @@
 terraform {
     required_providers {
-    ##https://registry.terraform.io/providers/hashicorp/random/latest/docs
-    # random = {
-    #   source = "hashicorp/random"
-    #   version = "3.5.1"
-    # }
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
     }
   }
-
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
-}
-
-
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "website_index" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-   etag = "${md5(file(var.index_html_filepath))}"
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag = "${md5(file(var.error_html_filepath))}"
-}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {}
