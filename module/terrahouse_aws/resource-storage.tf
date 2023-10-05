579314b6c9b07385cbcbcc667deba15e0b292a7e
MarinoVP <Marinovp@users.noreply.github.com>
2023-10-04 22:57:07 -0400

    #35 Uploading assets using for_each (#36)

--- module/terrahouse_aws/resource-storage.tf
+++ module/terrahouse_aws/resource-storage.tf
@@ -28,12 +28,29 @@ resource "aws_s3_object" "index_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-   etag = "${md5(file(var.index_html_filepath))}"
-   lifecycle {
-      replace_triggered_by = [terraform_data.content_version.output]
-    
-     ignore_changes = [etag]
+  etag = filemd5(var.index_html_filepath)
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
    }
+}
+
+
+#
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${var.assets_path}/${each.key}"
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("${var.assets_path}${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -46,7 +63,7 @@ resource "aws_s3_object" "error_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = "${md5(file(var.error_html_filepath))}"
+  etag = filemd5(var.error_html_filepath)
   lifecycle {
      ignore_changes = [etag]
    }
@@ -80,3 +97,5 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
   }
+
+  
