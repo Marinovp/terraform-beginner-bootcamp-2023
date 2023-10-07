e10ed043125e3bdc590366bfb9fe4362c5014a74
MarinoVP <Marinovp@users.noreply.github.com>
2023-10-07 15:49:36 -0400

    44 terratowns provider (#46)
    * #44 wip setting up skeleton for creating customer terraform provider
    
    * #44 implement tf provider block in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket nanme for static website"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket nanme for static website"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "Output of S3 Static Website"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "Output of S3 Static Website"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "The Cloudfront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "The Cloudfront Distribution Domain Name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
